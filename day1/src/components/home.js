4f94d9cd90abfa0a6f6ecd054b40c698d43b5d31
RheaAdh <rheadhikari@gmail.com>
2020-09-23 16:51:05 +0530

    FINAL

--- day1/src/components/home.js
+++ day1/src/components/home.js
@@ -29,7 +29,7 @@ export default class Home extends React.Component {
         />
         <button id="search">
           
-          Search {this.state.query}
+          Search
         </button>
       </form>
     );
